5642f14adc17a06087bbc6454a39140a327c10b4
ctoloczko <celina.toloczko@gmail.com>
2024-11-01 20:36:28 +0100

    added footer and styling to the app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import FactsGenerator from "./FactsGenerator";
 
 function App() {
   return (
-    <div className="app">
-      <FactsGenerator />
-    </div>
+    <>
+      <div className="app">
+        <FactsGenerator />
+
+        <footer>
+          <p class="text-center text-muted">
+            🩶 This project was coded by Celina Toloczko-Mastalerz and is{" "}
+            <a
+              href="https://github.com/ctoloczko"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced.
+            </a>
+          </p>
+        </footer>
+      </div>
+    </>
   );
 }
 
--- src/FactsGenerator.js
+++ src/FactsGenerator.js
@@ -5,11 +5,8 @@ import "./App.css";
 export default function FactsGenerator() {
   return (
     <>
-      <h2>
-        {" "}
-        Welcome to Fun Fact Frenzy! Tap to discover surprising facts you’ll want
-        to share!{" "}
-      </h2>
+      <h2> Welcome to Fun Fact Frenzy! </h2>
+      <h4>Tap to discover surprising facts you’ll want to share!</h4>
       <button>Hit Me with a Fact!</button>
 
       <Fact />
