c624f655d78f16500828496953cb08a083599a5e
ctoloczko <celina.toloczko@gmail.com>
2024-11-01 21:02:15 +0100

    styled button, imported spinner

--- src/Fact.js
+++ src/Fact.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Hourglass } from "react-loader-spinner";
 
 export default function Fact() {
-  return <p>Here comes the fact</p>;
+  return (
+    <>
+      <p className="Fact">Here comes the fact</p>
+      <Hourglass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="hourglass-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        colors={["#306cce", "#72a1ed"]}
+      />
+    </>
+  );
 }
